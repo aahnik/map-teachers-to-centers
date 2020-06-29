2960e7434cadd97043ed4062812ab90f521d7334
aahnik <aahnikdaw@gmail.com>
2020-06-29 18:28:21 +0530

    u

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from locatable import Teacher, ExamCenter
 import my_io
 
-
-
-
 exam_centers = my_io.generateSaveAndPlot(ExamCenter, 110, 'ExamCenters')
 vacancy = ExamCenter.total_vacancy(exam_centers)
 teachers = my_io.generateSaveAndPlot(Teacher, vacancy, 'Teachers')
